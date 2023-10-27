3ef7e9475843ec4dea00c835c04c7b8686b5d645
Mohammed Yusuf <mohammedyusuf445@gmail.com>
2023-10-27 10:10:46 +0300

    code

---
+++ server/migrations/versions/f822ce5836af_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: f822ce5836af
+Revises: 2da2c0ccb068
+Create Date: 2023-10-27 09:37:28.250652
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'f822ce5836af'
+down_revision = '2da2c0ccb068'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
